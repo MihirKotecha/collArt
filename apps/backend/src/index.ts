85653a66414c7271d895c9860f5bbf54bbb49825
MihirKotecha <mihir.k1@ahduni.edu.in>
2025-07-14 23:44:21 +0530

    Add Basic Struc for HTTP BE

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,32 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { authMidddleWare } from "./middleware";
 
 const app = express();
+export const JWT_TOKEN = "123";
 
-app.listen(3000,() => {
+app.post('/signup',(req,res) => {
+    
+});
+
+app.post('/signin',(req,res) => {
+    //@ts-ignore
+    const userId = req.userId;
+
+    const token = jwt.sign({
+        userId
+    },JWT_TOKEN)
+
+    res.json(token);
+});
+
+app.post('/room',authMidddleWare,(req,res) => {
+
+    res.json({
+        roomId: "123"
+    })
+});
+
+app.listen(4000,() => {
     console.log("Server running on port 3000!!");
 });
---
+++ apps/backend/src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_TOKEN } from ".";
+
+export const authMidddleWare = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+    const token = req.headers["authorization"] || "";
+
+    const decoded = jwt.verify(token,JWT_TOKEN);
+
+    if(decoded){
+        //@ts-ignore
+        req.userId = decoded.userId;
+        next();
+    }
+    else{
+        res.status(403).json({
+            message : "Unauthorized"
+        });
+    }
+};
