2b04e010dc9c48d633e9716655d50e73bc321ed1
MihirKotecha <mihir.k1@ahduni.edu.in>
2025-07-15 17:16:39 +0530

    Add authentication for WS requests

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,31 @@
 import { WebSocketServer } from "ws";
+import jwt  from "jsonwebtoken";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
-  console.log("Client connected to ws server on port 8080!!");
+wss.on("connection", (ws,request) => {
+  const url = request.url;
+
+  if(!url) {
+    return;
+  }
+
+  const urlParams = new URLSearchParams(url.split("?")[1]);
+  const token = urlParams.get("token");
+  const decoded = jwt.verify(token || "", "123");
+  
+  if(typeof decoded === 'string'){
+    ws.close(1008, "Invalid token");
+    return;
+  }
+
+  if(!decoded || !decoded.userId) {
+    ws.close();
+    return;
+  }
+
+  ws.send(`Token received: ${token}`);
+
   ws.on("message", (data) => {
     ws.send("Connected to the Web Socket Server!!");
   });
