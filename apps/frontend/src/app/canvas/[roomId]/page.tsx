e769f6fcdf18353b0883fcdb05ff76bee53459c7
MihirKotecha <mihir.k1@ahduni.edu.in>
2025-09-13 18:11:50 +0530

    Add init frontend with rect draw logic

---
+++ apps/frontend/src/app/canvas/[roomId]/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { initDraw } from "@/draw";
+import { useEffect, useRef } from "react";
+
+export default function drawingPage() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            initDraw(canvasRef.current);
+        }
+    }, [canvasRef]);
+
+    return (
+        <div>
+            <canvas ref={canvasRef}>
+            </canvas>
+        </div>
+    );
+}
