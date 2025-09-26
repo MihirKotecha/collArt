5e3ee782f2dc93919c2a7234baf0b6e2b466943d
MihirKotecha <mihir.k1@ahduni.edu.in>
2025-09-27 00:14:14 +0530

    draw logic for circle

--- apps/frontend/src/draw/Draw.ts
+++ apps/frontend/src/draw/Draw.ts
@@ -1,12 +1,12 @@
 import { Tool } from "@/components/CanvasComponent";
 import { getPastDrawings } from "@/lib/apiClient";
-import { RectSchemaType } from "@repo/common/types";
+import { EllipseSchemaType, RectSchemaType } from "@repo/common/types";
 
 export class Draw {
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
   private roomId: string;
-  private existingShapes: RectSchemaType[];
+  private existingShapes: (RectSchemaType | EllipseSchemaType)[];
   private socket: WebSocket;
   private startX: number = 0;
   private startY: number = 0;
@@ -14,7 +14,6 @@ export class Draw {
   private currTool: Tool = "rect";
 
   constructor(canvas: HTMLCanvasElement, roomId: string, socket: WebSocket) {
-    console.log('initialized');
     this.canvas = canvas;
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
@@ -54,7 +53,6 @@ export class Draw {
 
   initMouseHandlers() {
     this.canvas.addEventListener("mousedown", (e) => {
-      console.log('ldksjfioajsodijfi');
       this.clicked = true;
       this.startX = e.clientX;
       this.startY = e.clientY;
@@ -62,35 +60,76 @@ export class Draw {
 
     this.canvas.addEventListener("mousemove", (e) => {
       if (this.clicked) {
-        console.log('djsfojdio');
         const width = e.clientX - this.startX;
         const height = e.clientY - this.startY;
+        const midX = (e.clientX + this.startX) / 2;
+        const midY = (e.clientY + this.startY) / 2;
+        const radiusX = width / 2;
+        const radiusY = height / 2;
         this.clearCanvas();
-        this.ctx.strokeStyle = "#fff";
-        this.ctx.strokeRect(this.startX, this.startY, width, height);
+        if (this.currTool === "rect")
+          this.ctx.strokeRect(this.startX, this.startY, width, height);
+        else if (this.currTool === "circle") {
+          this.ctx.beginPath();
+          this.ctx.ellipse(
+            midX,
+            midY,
+            Math.abs(radiusX),
+            Math.abs(radiusY),
+            0,
+            0,
+            2 * Math.PI
+          );
+          this.ctx.strokeStyle = "#fff";
+          this.ctx.stroke();
+        }
       }
     });
 
     this.canvas.addEventListener("mouseup", (e) => {
-      console.log("triggg");
       this.clicked = false;
       const width = e.clientX - this.startX;
       const height = e.clientY - this.startY;
-      const shape: RectSchemaType = {
-        type: "rect",
-        x: this.startX,
-        y: this.startY,
-        width,
-        height,
-      };
-      this.existingShapes.push(shape);
-      this.socket.send(
-        JSON.stringify({
-          type: "chat",
-          roomId: this.roomId,
-          chat: JSON.stringify(shape),
-        })
-      );
+      if (this.currTool === "rect") {
+        const shape: RectSchemaType = {
+          type: "rect",
+          x: this.startX,
+          y: this.startY,
+          width,
+          height,
+        };
+        this.existingShapes.push(shape);
+        this.socket.send(
+          JSON.stringify({
+            type: "chat",
+            roomId: this.roomId,
+            chat: JSON.stringify(shape),
+          })
+        );
+      } else if (this.currTool === "circle") {
+        const midX = (e.clientX + this.startX) / 2;
+        const midY = (e.clientY + this.startY) / 2;
+        const radiusX = Math.abs(width / 2);
+        const radiusY = Math.abs(height / 2);
+        const ellipse: EllipseSchemaType = {
+          type: "ellipse",
+          x: midX,
+          y: midY,
+          radiusX,
+          radiusY,
+          rotation: 0,
+          startAngle: 0,
+          endAngle: 2 * Math.PI,
+        };
+        this.existingShapes.push(ellipse);
+        this.socket.send(
+          JSON.stringify({
+            type: "chat",
+            roomId: this.roomId,
+            chat: JSON.stringify(ellipse),
+          })
+        );
+      }
     });
   }
 
@@ -102,6 +141,20 @@ export class Draw {
       if (shape.type === "rect") {
         if (this.ctx == null) console.log("something");
         this.ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+      }
+      if (shape.type === "ellipse") {
+        this.ctx.beginPath();
+        this.ctx.ellipse(
+          shape.x,
+          shape.y,
+          shape.radiusX,
+          shape.radiusY,
+          shape.rotation,
+          shape.startAngle,
+          shape.endAngle
+        );
+        this.ctx.strokeStyle = "#fff";
+        this.ctx.stroke();
       }
     });
   }
