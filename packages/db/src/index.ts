0161fe1b9b4e03b21662c63bd79c3cfdbcc2537d
MihirKotecha <mihir.k1@ahduni.edu.in>
2026-06-21 18:42:52 +0530

    Upgrade prisma version

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,7 @@
-import { PrismaClient } from "@prisma/client";
-import { Prisma } from "@prisma/client";
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Prisma, PrismaClient } from "../generated/prisma/client.js";
 
-export const dbClient = new PrismaClient();
+const dbAdapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export const dbClient = new PrismaClient({ adapter: dbAdapter });
 export { Prisma };
